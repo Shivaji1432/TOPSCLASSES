80794eb1ee897279b568723d4b5ab1fbf3fac436
SHIVSHANKAR <shivshanji1432@gmail.com>
2025-04-01 12:24:46 +0530

    all

--- core_java/src/assignment1/Q20StringMethodE_C.java
+++ core_java/src/assignment1/Q20StringMethodE_C.java
@@ -5,9 +5,9 @@ public static void main(String[] args) {
 	String s="shiv";
 	String s1="shankar";
 	String s2="shiv";
-	System.out.println(s.equals(s2));
+	System.out.println(s.equals(s2));//compare the value given in string is equal or not.
 	System.out.println(s.equals(s1));
-	System.out.println(s.compareTo(s2));
-	System.out.println(s1.compareTo(s));
+	System.out.println(s.compareTo(s2));//here it compare the value given in string and
+	System.out.println(s1.compareTo(s));//gives 0 for true and +pve and -nve for false 
 }
 }
